e608c0c8638981ac73515b3b23103cffb7043323
boy-hack <34109680@qq.com>
2019-04-21 08:50:13 +0800

    update version to 1.1

--- HackRequests/__init__.py
+++ HackRequests/__init__.py
@@ -1,9 +1,9 @@
 __title__ = 'HackRequests'
-__version__ = '1.0'
+__version__ = '1.1'
 __author__ = 'w8ay'
 __author_email__ = 'w8ay@qq.com'
 __url__ = 'https://github.com/boy-hack/hack-requests/'
 __license__ = 'MIT'
-__copyright__ = 'Copyright 2018'
+__copyright__ = 'Copyright 2019'
 
 from HackRequests.HackRequests import *
